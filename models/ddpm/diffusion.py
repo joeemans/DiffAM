Added gradient checkpointing and other fixes to handle OOM - working kaggle notebook - our_readme.txt updated

models/ddpm/diffusion.py
@@ -1,6 +1,7 @@
 import math
 import torch
 import torch.nn as nn
+from torch.utils.checkpoint import checkpoint
 
 
 def get_timestep_embedding(timesteps, embedding_dim):
@@ -307,7 +308,7 @@ class DDPM(nn.Module):
         hs = [self.conv_in(x)]
         for i_level in range(self.num_resolutions):
             for i_block in range(self.num_res_blocks):
-                h = self.down[i_level].block[i_block](hs[-1], temb)
+                h = checkpoint(self.down[i_level].block[i_block], hs[-1], temb, use_reentrant=False)
                 if len(self.down[i_level].attn) > 0:
                     h = self.down[i_level].attn[i_block](h)
                 hs.append(h)
@@ -316,15 +317,15 @@ class DDPM(nn.Module):
 
         # middle
         h = hs[-1]
-        h = self.mid.block_1(h, temb)
+        h = checkpoint(self.mid.block_1, h, temb, use_reentrant=False)
         h = self.mid.attn_1(h)
-        h = self.mid.block_2(h, temb)
+        h = checkpoint(self.mid.block_2, h, temb, use_reentrant=False)
 
         # upsampling
         for i_level in reversed(range(self.num_resolutions)):
             for i_block in range(self.num_res_blocks + 1):
-                h = self.up[i_level].block[i_block](
-                    torch.cat([h, hs.pop()], dim=1), temb)
+                h = checkpoint(self.up[i_level].block[i_block],
+                    torch.cat([h, hs.pop()], dim=1), temb, use_reentrant=False)
                 if len(self.up[i_level].attn) > 0:
                     h = self.up[i_level].attn[i_block](h)
             if i_level != 0: